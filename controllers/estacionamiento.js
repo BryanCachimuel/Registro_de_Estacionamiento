9d32721ac0fbce83cc335a0f41da5113c096c138
BryanCachimuel <bryanloyo56@gmail.com>
2022-09-27 22:32:17 -0500

    probando la función render que redirige las funciones del controller hacia las vistas

--- controllers/estacionamiento.js
+++ controllers/estacionamiento.js
@@ -1,12 +1,6 @@
 const conexion = require('../database/db')
 
 exports.saludo = (req, res) => {
-    conexion.query('SELECT * FROM registro',(error,results) => {
-        if(error){
-            throw error;
-        }else{
-            res.send(results)
-        }
-    })
+   res.render('index')
 }
 
