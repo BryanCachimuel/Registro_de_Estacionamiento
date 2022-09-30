b763fc90d2d212ad80bd465e8a7250dcbc744f1b
BryanCachimuel <bryanloyo56@gmail.com>
2022-09-29 20:53:37 -0500

    instalando y configurando las dependencias para hacer el login y registro de usuarios del sistema

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const { json } = require('express');
 const express = require('express');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
 // configuración del motor de vistas
 app.set('view engine','ejs');
 
+// configuración de la carpeta public para archivos estáticos
+app.use(express.static('public'))
+
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
+
+// para procesar datos enviados desde formularios
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+// configuración de variables de entorno
+dotenv.config({path:'./env/.env'});
+
+// para poder trabajar con las cookies
+app.use(cookieParser);
 
 app.use('/', require('./routes/router'));
 
