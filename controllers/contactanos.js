edfd984be4348fb97bce3df73473487ae56a61d9
Bryan_Cachimuel <bryanloyo56@gmail.com>
2023-12-22 20:31:20 -0500

    realizando validaciones de los atributos que van a ser ingresados por el formulario de contactarse

--- controllers/contactanos.js
+++ controllers/contactanos.js
@@ -1,23 +1,39 @@
 const conexion = require('../database/db')
 
 exports.guardar = (req, res) => {
-    const nombre = req.body.nombre;
-    const email = req.body.email;
-    const telefono = req.body.telefono;
-    const encabezado = req.body.encabezado;
-    const observaciones = req.body.observaciones;
+    try {
+      const nombre = req.body.nombre;
+      const email = req.body.email;
+      const telefono = req.body.telefono;
+      const encabezado = req.body.encabezado;
+      const observaciones = req.body.observaciones;
 
-    conexion.query('INSERT INTO contactanos SET ?', {
-                    nombre:nombre,
-                    email:email,
-                    telefono:telefono,
-                    encabezado:encabezado,
-                    observaciones:observaciones},
-                    (error, results)=>{
-                        if(error){
-                           console.log(error);
-                        }else{
-                           res.redirect('/inicio');
-                        }
-                    })
+      if(!nombre || !email || !telefono || !encabezado || !observaciones){
+         res.render('inicio', {
+                alert:true,
+                alertTitle: "Advertencia",
+                alertMessage: "Ingrese un Nombre, Email, Teléfono, Encabezado y Observaciones",
+                alertIcon: "info",
+                showConfirmButton: true,
+                timer: false,
+                ruta: 'inicio'
+         })
+      }else{
+         conexion.query('INSERT INTO contactanos SET ?', {
+            nombre:nombre,
+            email:email,
+            telefono:telefono,
+            encabezado:encabezado,
+            observaciones:observaciones},
+            (error, results)=>{
+                  if(error){
+                     console.log(error);
+                  }else{
+                     res.redirect('/inicio');
+                  }
+            })
+      }     
+    } catch (error) {
+      console.log(error)
+    }
 }
